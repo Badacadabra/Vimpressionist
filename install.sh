4071ad8e76ae781e94f9c884c40034aefbca5ea8
Badacadabra <baptiste.vannesson@gmail.com>
2017-01-28 00:56:37 +0100

    Modify install script

--- install.sh
+++ install.sh
@@ -54,7 +54,10 @@ ask
 # PACKAGE MANAGEMENT 
 # =============================
 
+# /!\ Be careful here! 
+# For instance, pacman is not a package manager on Debian systems (it is the game...). 
 pkg_manager=""
+pkg_managers=( "pacman" "apt" "aptitude" "yum" "dnf" "zypper" "emerge" "equo" )
 
 test_pkg() {
     if hash "$1" 2> /dev/null; then
@@ -62,24 +65,80 @@ test_pkg() {
     fi
 }
 
-test_pkg "pacman"
-test_pkg "apt"
-test_pkg "yum"
-test_pkg "zypper"
+for mgr in "${pkg_managers[@]}"; do 
+    test_pkg "$mgr"
+done
 
 pkg_install() {
     # Arch & Co. 
-    if [ "$pkg_manager" = "pacman" ]; then
-        sudo pacman -S --noconfirm "$@"
+    if [ "$pkg_manager" = "pacman" ] && [ -f /etc/arch-release ]; then
+        if [ "$#" -eq 0 ]; then
+            sudo pacman -S --noconfirm i3 dmenu gvim vifm zathura zathura-pdf-mupdf termite mutt cmus lynx conky
+        else
+            sudo pacman -S --noconfirm "$@"
+        fi
     # Debian & Co. 
-    elif [ "$pkg_manager" = "apt" ]; then
-        sudo apt-get --assume-yes install "$@"
+    elif [ "$pkg_manager" = "apt" ] && [ -f /etc/debian_version ]; then
+        if [ "$#" -eq 0 ]; then
+            # Termite is not in official repos
+            git clone https://github.com/Corwind/termite-install.git
+            sh termite-install/termite-install.sh
+            sudo apt-get --assume-yes install i3 suckless-tools vim vim-gnome vifm zathura mutt cmus lynx conky
+        else
+            sudo apt-get --assume-yes install "$@"
+        fi
+    elif [ "$pkg_manager" = "aptitude" ] && [ -f /etc/debian_version ]; then
+        if [ "$#" -eq 0 ]; then
+            # Termite is not in official repos
+            git clone https://github.com/Corwind/termite-install.git
+            sh termite-install/termite-install.sh
+            sudo aptitude --assume-yes install i3 suckless-tools vim vim-gnome vifm zathura mutt cmus lynx conky
+        else
+            sudo aptitude --assume-yes install "$@"
+        fi
     # Red Hat & Co. 
-    elif [ "$pkg_manager" = "yum" ]; then
-        sudo yum --assumeyes install "$@"
-    # openSUSE
-    elif [ "$pkg_manager" = "zypper" ]; then
-        sudo zypper --non-interactive install "$@" 
+    elif [ "$pkg_manager" = "yum" ] && [ -f /etc/redhat-release ]; then
+        if [ "$#" -eq 0 ]; then
+            # Cmus and Termite are not in official repos
+            # Termite install???
+            su -lc 'yum install --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm' 
+            sudo yum --assumeyes install i3 dmenu vim-enhanced vim-X11 vifm zathura mutt cmus lynx conky
+        else
+            sudo yum --assumeyes install "$@"
+        fi
+    elif [ "$pkg_manager" = "dnf" ] && [ -f /etc/redhat-release ]; then
+        if [ "$#" -eq 0 ]; then
+            # Cmus and Termite are not in official repos
+            su -lc 'dnf install --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm' 
+            sudo dnf --assumeyes install i3 dmenu vim-enhanced vim-X11 vifm zathura mutt cmus lynx conky
+        else
+            sudo dnf --assumeyes install "$@"
+        fi
+    # SuSE & Co.
+    elif [ "$pkg_manager" = "zypper" ] && [ -f /etc/SuSE-release ]; then
+        if [ "$#" -eq 0 ]; then
+            # Zathura, Cmus and Termite are not in official repos
+            sudo zypper addrepo http://download.opensuse.org/repositories/home:msvec/openSUSE_Tumbleweed/home:msvec.repo
+            sudo zypper addrepo http://download.opensuse.org/repositories/home:cschneemann/openSUSE_Tumbleweed/home:cschneemann.repo
+            sudo zypper addrepo http://download.opensuse.org/repositories/home:cyphar/openSUSE_Tumbleweed/home:cyphar.repo
+            sudo zypper refresh
+            sudo zypper --non-interactive install i3 dmenu vim gvim vifm zathura termite mutt cmus lynx conky
+        else
+            sudo zypper --non-interactive install "$@"
+        fi
+    # Gentoo & Co.
+    # elif [ "$pkg_manager" = "emerge" ] && [ -f /etc/gentoo-release ]; then
+    #    if [ "$#" -eq 0 ]; then
+    #        sudo emerge install ...
+    #    else
+    #        sudo emerge install "$@"
+    #    fi
+    # elif [ "$pkg_manager" = "equo" ] && [ -f /etc/gentoo-release ]; then
+    #    if [ "$#" -eq 0 ]; then
+    #        sudo emerge install ...
+    #    else
+    #       sudo equo install "$@"
+    #    fi
     else
         log err "Your package manager has not been recognized."
         exit 2
@@ -118,7 +177,7 @@ fi
 # CORE 
 # =============================
 
-pkg_install i3 dmenu vim gvim vifm zathura termite mutt cmus lynx conky
+pkg_install
 
 git clone https://github.com/cknadler/vim-anywhere.git
 bash vim-anywhere/install
@@ -134,6 +193,8 @@ if [ "$pkg_manager" = "apt" ]; then
 else
     chromium https://chrome.google.com/webstore/detail/vimium/dbepggeogbaibhgnhhndojpepiihcmeb
 fi
+
+cp readline/inputrc ~/.inputrc
 
 # -----------------------------
 # i3
